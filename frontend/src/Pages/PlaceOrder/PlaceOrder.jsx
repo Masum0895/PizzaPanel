a56ac7e6c48f7e7382c36f39cbe524f34d0460a8
@Masum0895 <2221793@iub.edu.bd>
2025-07-28 23:44:35 +0600

    frontend complete

--- frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../Context/StoreContext';
 const PlaceOrder = () => {
+
+const {getTotalCartAmount}= useContext(StoreContext);
+
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className="title">Delivery information</p>
+        <div className="multi-field">
+          <input type="text" placeholder='First name' />
+          <input type="text" placeholder='Last name' />
+        </div>
+        <input type="email" placeholder='Email address'/>
+        <input type="text" placeholder='Street'/>
+        <div className="multi-field">
+          <input type="text" placeholder='Area' />
+          <input type="text" placeholder='City' />
+        </div>
+        <div className="multi-field">
+          <input type="text" placeholder='Zip Code' />
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type="text" placeholder='Phone No' />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr/>
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
+            </div>
+            <hr/>
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            </div>
+          </div>
+          <button >PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
